cf0dd662cb616a7636acb60fafb88fdcf16db9d4
iya-8986 <theauy98@gmail.com>
2025-01-07 14:09:34 +0800

    Created functions that will evaluate if the given input is valid

--- guess_number.py
+++ guess_number.py
@@ -1,41 +1,57 @@
+#-----------------GET THE IMPORT-----------------#
 import random
 
-
-while True:
-    try:
-        get_lowerbound = int(input("Enter the lowerbound: "))
-        get_upperbound = int(input("Enter the upperbound: "))
-        break
-    except:
-        continue
-
-random_number = random.randint(get_lowerbound,get_upperbound)
-guess_counter = 0
-print(random_number)
-
-while guess_counter <= 7:
-    guess_counter = guess_counter+1
+#-----------------FUNCTION THAT CHECK THE INPUT-----------------#
+def check_input(var_name):
     while True:
         try:
-            guess_number = int(input("Enter a number: "))
-            break
+            number = int(input(f"Enter the {var_name}: "))
+            return number
         except:
             continue
     
+#-----------------GET THE LOWERBOUND AND UPPERBOUND-----------------#
+def get_lowerbound():
+    lowerbound = check_input("lowerbound")
+    return lowerbound
+
+#-----------------GET THE UPPERBOUND AND UPPERBOUND-----------------#
+def get_upperbound():
+    upperbound = check_input("upperbound")
+    return upperbound
+
+
+
+#-----------------GET THE RANDOM NUMBER-----------------#
+def random_generator():
+    random_number = random.randint(get_lowerbound(),get_upperbound())
+    return random_number
+
+def game():
+    guess_counter = 0
+    random_number = random_generator()
+    while guess_counter <= 7:
+
+        guess_counter = guess_counter+1
+        
+        guess_number = check_input("guess number")
+
+        
+        
+        if guess_number == random_number:
+            print(f"The number is {guess_number} and you found it right!! in {guess_counter} attempts")
+            break
+
+        elif guess_counter == 7 and guess_number != random_number:
+            print(f"Oops sorry, the number is {random_number} better luck next time")
+            break
+
+        elif guess_number > random_number:
+            print("Your guess is high")
+
+        elif guess_number < random_number:
+            print("Your guess is low")
     
-    if guess_number == random_number:
-        print(f"The number is {guess_number} and you found it right!! in {guess_counter} attempt")
-        break
-
-    elif guess_counter == 7 and guess_number != random_number:
-        print(f"Oops sorry, the number is {random_number} better luck next time")
-        break
-
-    elif guess_number > random_number:
-        print("Your guess is high")
-
-    elif guess_number < random_number:
-        print("Your guess is low")
+game()    
 
 
-    
